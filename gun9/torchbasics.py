321945d2d71ce5c13a2df0723f1e2f0bfc891cd7
emreates29 <emreates037@gmail.com>
2025-08-06 16:46:09 +0300

    Update torchbasics.py

--- gun9/torchbasics.py
+++ gun9/torchbasics.py
@@ -1,24 +1,24 @@
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Cihaz: {device}")
+print(f"cihaz: {device}")
 
 a = torch.randn(3, 3, device=device)
 b = torch.randn(3, 3, device=device)
-print("Tensor a:\n", a)
-print("Tensor b:\n", b)
+print("tensor a:\n", a)
+print("tensor b:\n", b)
 
 sum_tensor = a + b
-print("\nToplam:\n", sum_tensor)
+print("\ntoplam:\n", sum_tensor)
 
 matmul_result = torch.matmul(a.T, b)
-print("\nMatris çarpımı a.T @ b:\n", matmul_result)
+print("\nmatris çarpımı a.T @ b:\n", matmul_result)
 
 relu_tensor = torch.relu(a)
 print("\nReLU uygulanmış a:\n", relu_tensor)
 
 reshaped = a.view(-1)
-print("\nYeniden şekillendirilmiş (flattened) a:\n", reshaped)
+print("\nyeniden şekillendirilmiş a:\n", reshaped)
 
 x = torch.randn(3, 3, requires_grad=True, device=device)
 w = torch.randn(3, 2, requires_grad=True, device=device)
